b7417aa1cc082a46eda6e4066ffbcdaec50d7e99
David Ren <dren391@gmail.com>
2020-05-04 17:57:30 -0400

    Updates

---
+++ precache-manifest.678ea97dfd9da5fc3cb7f399dbd6fdb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759c48d6be187e257f8d7bcb347439bf",
+    "url": "/react-eventslist-app/index.html"
+  },
+  {
+    "revision": "16d39e6e3f5586d95563",
+    "url": "/react-eventslist-app/static/css/main.87eb1ac3.chunk.css"
+  },
+  {
+    "revision": "9a735dd7eebc7182d6e1",
+    "url": "/react-eventslist-app/static/js/2.efae118c.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/react-eventslist-app/static/js/2.efae118c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16d39e6e3f5586d95563",
+    "url": "/react-eventslist-app/static/js/main.6cf7bf86.chunk.js"
+  },
+  {
+    "revision": "fbb96b4c836c734eb1cc",
+    "url": "/react-eventslist-app/static/js/runtime-main.5b1cbaf7.js"
+  },
+  {
+    "revision": "17ba27464fce57e571a71b2f40463937",
+    "url": "/react-eventslist-app/static/media/elton-john-background.17ba2746.jpg"
+  }
+]);
